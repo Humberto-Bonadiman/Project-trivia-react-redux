9f4a8c992fb3b2bf55d55a827c43c6da10c1a718
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-10-21 17:33:49 -0300

    12. resolvendo

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import md5 from 'crypto-js/md5';
+import { connect } from 'react-redux';
+
+const GRAVATAR_URL = 'https://www.gravatar.com/avatar/';
+const EMAIL_TO_HASH = (email) => md5(email).toString();
+
+class Header extends Component {
+  render() {
+    const { name, gravatarEmail } = this.props;
+
+    return (
+      <header>
+        <img
+          src={ `${GRAVATAR_URL}${EMAIL_TO_HASH(gravatarEmail)}` }
+          alt="imagem do gravatar"
+          data-testid="header-profile-picture"
+        />
+        <span data-testid="header-player-name">{ name }</span>
+        <span data-testid="header-score">0</span>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  name: state.user.name,
+  gravatarEmail: state.user.gravatarEmail,
+});
+
+export default connect(mapStateToProps)(Header);
